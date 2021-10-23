979cddcdbeee13b0ec5149babf5dc0aebadf9f87
Michael Zhao <michaelzhao@college.harvard.edu>
2021-10-23 17:45:45 -0400

    Office hours commit

---
+++ src/CardEditor.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class CardEditor extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Card Editor</h2>
+            </div>
+        );
+    }
+}
+
+export default CardEditor;
